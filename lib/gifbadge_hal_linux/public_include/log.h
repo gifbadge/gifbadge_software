661de42025f0c86d08def8be1ef95cb9a7afc47e
gifbadge <gifbadge@gifbadge.ca>
2026-02-22 14:11:54 -0500

    Add LOGD macro

--- lib/gifbadge_hal_linux/public_include/log.h
+++ lib/gifbadge_hal_linux/public_include/log.h
@@ -7,5 +7,6 @@
 #pragma once
 
 #include "console.h"
+#define DEBUG 0
 #define LOGI(tag, format, ...) console_print(tag, format, ##__VA_ARGS__)
-
+#define LOGD(tag, format, ...) do { if (DEBUG) console_print(tag, format, ##__VA_ARGS__); } while (0)
