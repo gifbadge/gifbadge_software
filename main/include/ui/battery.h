db68b6e9ae5ad727822a7ea488e92de82b89662d
gifbadge <gifbadge@gifbadge.ca>
2026-03-08 13:49:48 -0400

    Move battery display to it's own file

---
+++ main/include/ui/battery.h
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2026 GifBadge
+ *
+ * SPDX-License-Identifier:   GPL-3.0-or-later
+ ******************************************************************************/
+
+#pragma once
+
+#include <lvgl.h>
+
+void battery_widget(lv_obj_t *scr);
--- main/ui/menu.cpp
+++ main/ui/menu.cpp
@@ -17,14 +17,13 @@
 #include <input.h>
 #include <ui/usb_connected.h>
 
-#include "hal/battery.h"
 #include "ui/main_menu.h"
 #include "ui/style.h"
 #include "hw_init.h"
 #include "display.h"
-#include "ui/widgets/battery/lv_battery.h"
 #include "timers.h"
 #include "touch.h"
+#include "ui/battery.h"
 
 static const char *TAG = "MENU";
 
@@ -90,102 +89,6 @@ void lvgl_close() {
   LOGI(TAG, "Close Done");
 }
 
-void battery_percent_update(lv_obj_t *widget) {
-  auto battery = static_cast<hal::battery::Battery *>(lv_obj_get_user_data(widget));
-  if (battery->BatteryStatus() == hal::battery::Battery::State::ERROR || battery->BatteryStatus() == hal::battery::Battery::State::NOT_PRESENT) {
-    lv_battery_set_value(widget, 0);
-  } else {
-    lv_battery_set_value(widget, battery->BatterySoc());
-  }
-}
-
-void battery_symbol_update(lv_obj_t *cont) {
-  auto battery = static_cast<hal::battery::Battery *>(lv_obj_get_user_data(cont));
-  lv_obj_t *symbol = lv_obj_get_child(cont, 0);
-  lv_obj_set_style_bg_opa(cont, LV_OPA_COVER, LV_PART_MAIN);
-  lv_obj_remove_flag(cont, LV_OBJ_FLAG_HIDDEN);
-  if (battery->BatteryStatus() == hal::battery::Battery::State::ERROR) {
-    lv_image_set_src(symbol, LV_SYMBOL_DUMMY "\uf22f");
-    lv_obj_set_style_text_color(symbol, lv_color_hex(0xeed202), LV_PART_MAIN); //Yellow
-  } else if (battery->BatteryStatus() == hal::battery::Battery::State::NOT_PRESENT) {
-    lv_image_set_src(symbol, LV_SYMBOL_DUMMY "\ue5cd");
-    lv_obj_set_style_text_color(symbol, lv_color_hex(0xff0033), LV_PART_MAIN); //Red
-  } else if (battery->BatteryStatus() == hal::battery::Battery::State::CHARGING) {
-    lv_image_set_src(symbol, LV_SYMBOL_DUMMY "\uea0b");
-    lv_obj_set_style_text_color(symbol, lv_color_hex(0x50C878), LV_PART_MAIN); //Green
-  } else {
-    lv_image_set_src(symbol, LV_SYMBOL_DUMMY);
-    lv_obj_add_flag(cont, LV_OBJ_FLAG_HIDDEN);
-    lv_obj_set_style_bg_opa(cont, LV_OPA_TRANSP, LV_PART_MAIN);
-  }
-}
-
-
-
-static void battery_widget(lv_obj_t *scr) {
-  lv_obj_t *battery_bar = lv_obj_create(scr);
-  lv_obj_set_size(battery_bar, lv_pct(100), LV_SIZE_CONTENT);
-  lv_obj_add_style(battery_bar, &style_battery_bar, LV_PART_MAIN);
-
-  lv_obj_t *bar = lv_battery_create(battery_bar);
-  lv_obj_add_style(bar, &style_battery_indicator, LV_PART_INDICATOR);
-  lv_obj_add_style(bar, &style_battery_main, LV_PART_MAIN);
-  lv_obj_set_size(bar, 20, 40);
-  lv_obj_center(bar);
-  lv_obj_set_user_data(bar, get_board()->GetBattery());
-
-
-  lv_obj_add_event_cb(bar, [](lv_event_t *e) {
-    battery_percent_update(static_cast<lv_obj_t *>(lv_event_get_target(e)));
-  }, LV_EVENT_REFRESH, nullptr);
-  battery_percent_update(bar);
-
-  lv_timer_t *timer = lv_timer_create([](lv_timer_t *timer) {
-    auto *obj = static_cast<lv_obj_t *>(timer->user_data);
-    lv_obj_send_event(obj, LV_EVENT_REFRESH, nullptr);
-  }, 30000, bar);
-  lv_obj_add_event_cb(bar, [](lv_event_t *e) {
-    auto *timer = static_cast<lv_timer_t *>(e->user_data);
-    lv_timer_del(timer);
-  }, LV_EVENT_DELETE, timer);
-
-  lv_obj_t *battery_symbol_cont = lv_obj_create(battery_bar);
-  lv_obj_remove_style_all(battery_symbol_cont);
-  lv_obj_add_style(battery_symbol_cont, &style_battery_icon_container, LV_PART_MAIN);
-  lv_obj_set_size(battery_symbol_cont, 24, 24);
-  lv_obj_align_to(battery_symbol_cont, bar, LV_ALIGN_BOTTOM_RIGHT, 18, 12);
-
-  lv_obj_t *battery_symbol = lv_img_create(battery_symbol_cont);
-  lv_obj_add_style(battery_symbol, &style_battery_icon, LV_PART_MAIN);
-  lv_obj_align(battery_symbol, LV_ALIGN_CENTER, 0, 0);
-
-  lv_obj_set_user_data(battery_symbol_cont, get_board()->GetBattery());
-
-  lv_obj_add_event_cb(battery_symbol_cont, [](lv_event_t *e) {
-    battery_symbol_update(static_cast<lv_obj_t *>(lv_event_get_target(e)));
-  }, LV_EVENT_REFRESH, nullptr);
-  battery_symbol_update(battery_symbol_cont);
-
-  lv_timer_t *timer_icon = lv_timer_create([](lv_timer_t *timer) {
-    auto *obj = static_cast<lv_obj_t *>(timer->user_data);
-    lv_obj_send_event(obj, LV_EVENT_REFRESH, nullptr);
-  }, 30000, battery_symbol_cont);
-  lv_obj_add_event_cb(battery_symbol_cont, [](lv_event_t *e) {
-    auto *timer = static_cast<lv_timer_t *>(e->user_data);
-    lv_timer_del(timer);
-  }, LV_EVENT_DELETE, timer_icon);
-
-}
-
-void battery_draw() {
-  lv_group_t *g = lv_group_create();
-  lv_group_set_default(g);
-  lv_indev_set_group(lvgl_encoder, g);
-  lv_obj_t *scr = create_screen();
-  lv_screen_load(scr);
-  battery_widget(lv_layer_top());
-}
-
 static void lvgl_wake_up() {
     LOGI(TAG, "Wakeup");
     get_board()->PmLock();
@@ -197,6 +100,14 @@ static void lvgl_wake_up() {
 
     xSemaphoreGive(flushSem);
     LOGI(TAG, "Wakeup Done");
+}
+
+static void lvgl_screen_init() {
+  lv_group_t *g = lv_group_create();
+  lv_group_set_default(g);
+  lv_indev_set_group(lvgl_encoder, g);
+  lv_obj_t *scr = create_screen();
+  lv_screen_load(scr);
 }
 
 void task(void *) {
@@ -226,7 +137,7 @@ void task(void *) {
         display_task_handle = xTaskGetHandle("display_task");
         xTaskNotifyIndexed(display_task_handle, 0, DISPLAY_MENU, eSetValueWithOverwrite);
         xTaskNotifyWaitIndexed(0, 0, 0xffffffff, &option, portMAX_DELAY);
-        battery_draw();
+        lvgl_screen_init();
         main_menu();
         LOGI(TAG, "LVGL_RESUME");
         break;
@@ -236,7 +147,8 @@ void task(void *) {
         display_task_handle = xTaskGetHandle("display_task");
         xTaskNotifyIndexed(display_task_handle, 0, DISPLAY_NOTIFY_USB, eSetValueWithOverwrite);
         xTaskNotifyWaitIndexed(0, 0, 0xffffffff, &option, portMAX_DELAY);
-        battery_draw();
+        lvgl_screen_init();
+        battery_widget(lv_layer_top());
         lvgl_usb_connected();
         LOGI(TAG, "LVGL_RESUME");
         break;
@@ -323,10 +235,6 @@ void lvgl_init(Boards::Board *board) {
   }
 
 }
-
-
-
-
 
 void lvgl_menu_open() {
   xTaskNotifyIndexed(lvgl_task, 0, LVGL_RESUME_MENU, eSetValueWithOverwrite);
